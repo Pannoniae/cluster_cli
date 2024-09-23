81e0bc285d8dc5b474ec1484ca8385286beb5ed3
Pannoniae <25565223+Pannoniae@users.noreply.github.com>
2024-09-23 20:33:29 +0100

    simplify

--- src/main/groovy/cluster_cli/records/ExtractVersion.groovy
+++ src/main/groovy/cluster_cli/records/ExtractVersion.groovy
@@ -2,18 +2,7 @@ package cluster_cli.records
 
 class ExtractVersion {
   static boolean extractVersion(String version, String nature ) {
-    if (1 == 1 || nature == "Net")
-      return true
-    else {
-      String userHome = System.getProperty("user.home")
-      String jarLocation = "${userHome}\\.m2\\repository\\jonkerridge\\cluster_cli"
-      String gradleLocation = "${userHome}\\.gradle\\caches\\modules-2\\files-2.1\\jonkerridge\\cluster_cli"
-      String folder = gradleLocation + "\\$version"
-      if (new File(folder).isDirectory()) return true else {
-        folder = jarLocation + "\\$version"
-        return new File(folder).isDirectory()
-      }
-    }
+    return true
   }
 
   static void main(String[] args) {
